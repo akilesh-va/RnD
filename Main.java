1614c69b29ead9809b804a262d0220739acb9a4e
akilesh-va <akilesh.va.266@gmail.com>
2025-03-08 07:30:29 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
-public class Main{
+Dpublic class Main{
   public static void main(String str[]) {
     RoomTempratureControl rtControl = new RoomRempratireControl();
-    rtControl.turnHeaterOn(Double.valueOf(str[0]), Double.valueOf(str[1]), RoomType.valueOf(str[2));
+    rtControl.turnHeaterOn(Double.valueOf(str[0]), Double.valueOf(str[1]), RoomType.BED);
   }
 }
